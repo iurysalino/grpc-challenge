1c430cd0a9d4420cf6cf7f23ac199d67b7171ee4
iurysalino <iurysalino@gmail.com>
2022-02-22 22:28:03 -0300

    Code format.

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -7,7 +7,7 @@ import service.ProductService;
 
 public class Server {
   public static void main(String[] args) throws InterruptedException, IOException {
-    int port =  Integer.parseInt(args[0]);
+    int port = Integer.parseInt(args[0]);
     io.grpc.Server server = ServerBuilder.forPort(port).addService(new ProductService()).build();
     try {
       server.start();
