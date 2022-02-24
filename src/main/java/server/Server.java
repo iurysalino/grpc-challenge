850da8c892e22e8ebe4a23f2eb52303c104b29ce
iurysalino <iurysalino@gmail.com>
2022-02-24 18:39:51 -0300

    Include Java Dec.

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,11 +1,16 @@
 package server;
 
-import java.io.IOException;
-
 import io.grpc.ServerBuilder;
+import java.io.IOException;
 import service.ProductService;
 
+/**
+ * Class to initialize the Server.
+ */
 public class Server {
+  /**
+   * Main method of the Class to initialize the Server.
+   */
   public static void main(String[] args) throws InterruptedException, IOException {
     int port = Integer.parseInt(args[0]);
     io.grpc.Server server = ServerBuilder.forPort(port).addService(new ProductService()).build();
